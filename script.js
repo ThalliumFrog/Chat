c72f59d5b457b2fc735dfc05cf09222b97554d65
Thallium <128066196+ThalliumFrog@users.noreply.github.com>
2023-03-23 18:40:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ alert("If one uses extreme language their account will be terminated.");
 
 passwordatmpt = prompt("Enter Password");
 
-scr1="Z2dzY3J1Yno=";prpro=atob(scr1);scr1="clr";
-if (passwordatmpt==prpro) {username = prompt("Enter Username");CLIENT_IDx = 'hWmT5ZuGe51IgiMD';}
-prpro = "clr";
-
-scr2="bW9kZXJhdG9y=";mdrtr=atob(scr2);scr2="clr";
-if (passwordatmpt==mdrtr) {username = prompt("Enter Username");)
-mdrtr = "clr";
-
 CLIENT_IDx = 'HbfBa2vZG45ihRaF';
 
 function getName() { 
@@ -33,6 +25,12 @@ function getName() {
   eptme="TmVvblllbGxvdzc2ODgxMw==";passwordtuckerm=atob(eptme);eptme="clr";
   if (passwordatmpt==passwordtuckerm) {username = "Tucker";}
   passwordtuckerm = "clr";
+  scr1="Z2dzY3J1Yno=";prpro=atob(scr1);scr1="clr";
+  if (passwordatmpt==prpro) {username = prompt("Enter Username");CLIENT_IDx = 'hWmT5ZuGe51IgiMD';}
+  prpro = "clr";
+  scr2="bW9kZXJhdG9y=";mdrtr=atob(scr2);scr2="clr";
+  if (passwordatmpt==mdrtr) {username = prompt("Enter Username");)
+  mdrtr = "clr";
   return username
 }
 
